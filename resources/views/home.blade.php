5e265b52c4fd4e52bf92f85777ba8984822c1a1f
soumya kesarwani <soumyakesrwani70@gmail.com>
2025-03-11 19:32:47 +0530

    top deals

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,16 @@
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="..." class="d-block w-100" alt="...">
+        <img src="{{ asset('asset/images/slider1.png') }}" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
+        <img src="{{ asset('asset/images/slider2.png') }}" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
+        <img src="{{ asset('asset/images/slider3.png') }}" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="{{ asset('asset/images/slider4.png') }}" class="d-block w-100" alt="...">
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -22,4 +25,58 @@
       <span class="visually-hidden">Next</span>
     </button>
   </div>
+{{--  product section start  --}}
+  <section class="my-5">
+   
+    
+      <div class="container"> 
+        <div class="d-flex bd-highlight mb-3">
+          <div class="me-auto bd-highlight"> 
+            <h2>Top Deals</h2>
+          </div>
+          <div class="p-2 bd-highlight">
+            <a href="#"class="btn btn-sm theme-green-btn text-light">View All</a></div>
+          
+        </div>
+                 
+             
+          <div class="row theme-product">
+            <div class="col-md-3">
+              <div class="card">
+                <div class="card-body">
+                  <a href="#"class="text-decoration-none"><img src="{{ asset('asset/images/products/1.jpg') }}" class="card-img-top" alt="..."></a>
+                    <h6 class="card-title text-center"> <a href="#"class="text-decoration-none">Campus Shoes</a></h6>
+                    <h5 class="card-title text-center">₹  4,999</h5>
+                </div>
+              </div>
+          </div>
+              <div class="col-md-3">
+                  <div class="card">
+                    <div class="card-body">
+                      <a href="#"class="text-decoration-none"><img src="{{ asset('asset/images/products/2.jpg') }}" class="card-img-top" alt="..."></a>
+                        <h6 class="card-title text-center"> <a href="#"class="text-decoration-none">Apple Watch</a></h6>
+                        <h5 class="card-title text-center">₹  47,999</h5>
+                    </div>
+                  </div>
+              </div>
+              <div class="col-md-3">
+                <div class="card">
+                  <div class="card-body">
+                    <a href="#"class="text-decoration-none"><img src="{{ asset('asset/images/products/3.jpg') }}" class="card-img-top" alt="..."></a>
+                      <h6 class="card-title text-center"> <a href="#"class="text-decoration-none">Nike Cap</a></h6>
+                      <h5 class="card-title text-center">₹  2,999</h5>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3">
+              <div class="card">
+                <div class="card-body">
+                  <a href="#" class="text-decoration-none"><img src="{{ asset('asset/images/products/4.jpg') }}" class="card-img-top" alt="..."></a>
+                    <h6 class="card-title text-center text-dark"> <a href="#"class="text-decoration-none ">Wooden Chair</a></h6>
+                    <h5 class="card-title text-center">₹  14,999</h5>
+                </div>
+              </div>
+              </div>
+          </div>
+  </section>
 @endsection
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="text-center bg-light p-3 theme-footer ">
+<footer class="text-center  p-3 theme-footer ">
     © 2024 Ecommerce. All Rights Reserved.
   </footer>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
