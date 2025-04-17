de8ba86a6134fc312157e4a55dcdd00391f9dfc1
Soumya kesarwani <68148283+soumya23463@users.noreply.github.com>
2025-04-17 22:06:44 +0530

    twenty four

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -9,21 +10,32 @@ class ShopController extends Controller
 {
     public function index(Request $request)
     {
-
-        $size = $request->query('size') ? $request->query('size') : 2;
-
-        $order = $request->query('order') ? $request->query('order') : -1;
+        $size = $request->query('size') ? $request->query('size') : 12;
+        $order = $request->query('order') ? $request->query('order') : 'default';
+        $f_brands = $request->query('brands');
         if ($order == 'date') {
-            $products = Product::orderBy('created_at', 'DESC')->paginate($size);
+            $products = Product::where(function ($query) use ($f_brands) {
+                $query->whereIn('brand_id', explode(',', $f_brands))->orWhereRaw("'" . $f_brands . "' = ''");
+            })
+                ->orderBy('created_at', 'DESC')->paginate($size);
         } else if ($order == "price") {
-            $products = Product::orderBy('regular_price', 'ASC')->paginate($size);
+            $products = Product::where(function ($query) use ($f_brands) {
+                $query->whereIn('brand_id', explode(',', $f_brands))->orWhereRaw("'" . $f_brands . "' = ''");
+            })
+                ->orderBy('regular_price', 'ASC')->paginate($size);
         } else if ($order == "price-desc") {
-            $products = Product::orderBy('regular_price', 'DESC')->paginate($size);
+            $products = Product::where(function ($query) use ($f_brands) {
+                $query->whereIn('brand_id', explode(',', $f_brands))->orWhereRaw("'" . $f_brands . "' = ''");
+            })
+                ->orderBy('regular_price', 'DESC')->paginate($size);
         } else {
-            $products = Product::paginate($size);
+            $products = Product::where(function ($query) use ($f_brands) {
+                $query->whereIn('brand_id', explode(',', $f_brands))->orWhereRaw("'" . $f_brands . "' = ''");
+            })
+                ->paginate($size);
         }
-
-        return view('shop', compact("products", "size", "order"));
+        $brands = Brand::orderBy("name", "ASC")->get();
+        return view('shop', compact("products", "size", "order", "brands", "f_brands"));
     }
 
     public function product_details($product_slug)
